4899595e2c9753cc4db4cad7aef4711b121dfee9
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2020-03-10 14:12:02 -0300

    finalizando trabalho 1

--- 1-AnaliseAssintoticaERecursão/jogoDasBolhas.c
+++ 1-AnaliseAssintoticaERecursão/jogoDasBolhas.c
@@ -1,12 +1,57 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void recebeEntradas(int vetor[], int tamanhoDoVetor){
+    for(int i=0; i<tamanhoDoVetor; i++){
+        scanf("%d", &vetor[i]);
+    }
+    printf("\n");
+}
+
+void imprimeEntradas(int vetor[], int tamanhoDoVetor){
+    for(int i=0; i<tamanhoDoVetor; i++){
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+}
+
+int bubbleSort(int vetor[], int tamanhoDoVetor){
+    int count = 0;
+    for(int i = tamanhoDoVetor -1; i > 0; i--){
+        for(int j = 0; j < i; j++){
+            if(vetor[j] > vetor[j+1]){
+                int temp = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = temp;
+                count = count + 1;
+            }
+        }
+    }
+
+    return count;
+}
+
+void exibirVencedor(int resultado){
+    printf("%d \n", resultado);
+    if(resultado%2 == 0){
+        printf("Fulano");
+    }else{
+        printf("Ciclano");
+    }
+}
+
 int main(){
     int numeroDeEntradas = 0;
-    printf('Numero de entradas: ');
-    scanf('%d', &numeroDeEntradas);
+    printf("Numero de entradas: ");
+    scanf("%d", &numeroDeEntradas);
 
     int vetorSequencia[numeroDeEntradas];
+    recebeEntradas(vetorSequencia, numeroDeEntradas);
+    
+    int resultado = bubbleSort(vetorSequencia, numeroDeEntradas);
+    imprimeEntradas(vetorSequencia, numeroDeEntradas);
 
-    return 0
+    exibirVencedor(resultado);
+
+    return 0;
 }
