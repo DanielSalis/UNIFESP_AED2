4d9f23486b436ad706fca781b021c7b002a9fcef
DanielSalis <danielsalis98@gmail.com>
2020-03-10 09:17:52 -0300

    jogo das bolhas

---
+++ 1-AnaliseAssintoticaERecursão/jogoDasBolhas.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int numeroDeEntradas = 0;
+    printf('Numero de entradas: ');
+    scanf('%d', &numeroDeEntradas);
+
+    int vetorSequencia[numeroDeEntradas];
+
+    return 0
+}
