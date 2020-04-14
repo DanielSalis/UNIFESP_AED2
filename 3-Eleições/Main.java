aa94ad9fb1a026b759ee68c80a760140c7aaf6b7
Daniel Salis <danielsalis98@gmail.com>
2020-04-14 20:54:25 -0300

    .

--- 3-Eleições/Main.java
+++ 3-Eleições/Main.java
@@ -10,18 +10,21 @@ public class Main {
         int votosRecebidosSegundoTurno;
         float pctDeVotosPrimeiroTurno;
         float pctDeVotosSegundoTurno;
+        int totalVotosInvalidosSegundoTurno = 0;
+        boolean temAlgumVoto = false;
     }
 
     List<Candidato> Candidatos = new ArrayList<Candidato>();
     int numEleitores;
-    int numVotos;
+    int numCandidatos;
+    int numVotos = 3;
     int[][] votesMatrix;
 
     public Main(int lines, int columns) {
         this.numEleitores = lines;
-        this.numVotos = columns;
+        this.numCandidatos = columns;
 
-        for (int i = 0; i < columns; i++) {
+        for (int i = 0; i < numCandidatos; i++) {
             Candidato novoCandidato = new Candidato();
             novoCandidato.id = i + 1;
             novoCandidato.votosRecebidosPrimeiroTurno = 0;
@@ -66,39 +69,113 @@ public class Main {
             }
         }
 
+        boolean todosOsCandidatosSaoInvalidos = true;
         for (Candidato candidato : Candidatos) {
-            candidato.pctDeVotosPrimeiroTurno = (float) candidato.votosRecebidos / (float) totalDeVotosValidos;
+            candidato.pctDeVotosPrimeiroTurno = (float) candidato.votosRecebidosPrimeiroTurno
+                    / (float) totalDeVotosValidos;
             if (candidato.pctDeVotosPrimeiroTurno >= 0.5) {
-                System.out.println(candidato.id + ": " + candidato.pctDeVotosPrimeiroTurno);
+                todosOsCandidatosSaoInvalidos = false;
+                System.out.println(candidato.id + ": " + candidato.pctDeVotosPrimeiroTurno * 100);
                 return;
+            }
+            if (candidato.pctDeVotosPrimeiroTurno > 0) {
+                todosOsCandidatosSaoInvalidos = false;
             }
         }
 
-        // OrdenarVetor de Candidatos
-        // pegar os dois com a maior porcentagem do primeiro turno
+        if (todosOsCandidatosSaoInvalidos) {
+            System.out.println(0);
+            return;
+        }
 
-        getResultadosSegundoTurno(candidato_1, candidato_2);
+        // OrdenarVetor de Candidatos
+        List<Candidato> candidatosOrdenados = selectionSort(Candidatos);
+        getResultadosSegundoTurno(candidatosOrdenados.get(candidatosOrdenados.size() - 1),
+                candidatosOrdenados.get(candidatosOrdenados.size() - 2));
+
     }
 
     public void getResultadosSegundoTurno(Candidato candidato_1, Candidato candidato_2) {
-        int totalDeVotosValidos = 0;
+
         for (int i = 0; i < numEleitores; i++) {
             for (int j = 0; j < numVotos; j++) {
-                if (candidato_1.id == votesMatrix[i][j]) {
-                    totalDeVotosValidos++;
-                    candidato_1.votosRecebidosSegundoTurno++;
-                    break;
-                } else if (candidato_2.id == votesMatrix[i][j]) {
-                    totalDeVotosValidos++;
+                candidato_2.temAlgumVoto = false;
+                if (candidato_2.id == votesMatrix[i][j]) {
+                    candidato_2.temAlgumVoto = true;
                     candidato_2.votosRecebidosSegundoTurno++;
+                    break;
+                } else if (candidato_1.id == votesMatrix[i][j]) {
+                    candidato_1.temAlgumVoto = true;
+                    candidato_1.votosRecebidosSegundoTurno++;
                     break;
                 }
             }
         }
+
+        int totalDeVotosValidos = numEleitores;
+        for (int i = 0; i < numEleitores; i++) {
+            for (int j = 0; j < numVotos; j++) {
+                candidato_1.temAlgumVoto = false;
+                if (candidato_1.id == votesMatrix[i][j]) {
+                    candidato_1.temAlgumVoto = true;
+                    break;
+                }
+            }
+            if (candidato_1.temAlgumVoto == false) {
+                candidato_1.totalVotosInvalidosSegundoTurno++;
+            }
+        }
+
+        for (int i = 0; i < numEleitores; i++) {
+            for (int j = 0; j < numVotos; j++) {
+                candidato_2.temAlgumVoto = false;
+                if (candidato_2.id == votesMatrix[i][j]) {
+                    candidato_2.temAlgumVoto = true;
+                    break;
+                }
+            }
+            if (candidato_2.temAlgumVoto == false) {
+                candidato_2.totalVotosInvalidosSegundoTurno++;
+            }
+        }
+
+        List<Candidato> candidatosMaisVotados = new ArrayList<Candidato>();
+        candidatosMaisVotados.add(candidato_1);
+        candidatosMaisVotados.add(candidato_2);
+
+        for (Candidato candidato : candidatosMaisVotados) {
+            candidato.pctDeVotosSegundoTurno = (float) candidato.votosRecebidosSegundoTurno
+                    / (float) (totalDeVotosValidos - candidato.totalVotosInvalidosSegundoTurno);
+        }
+
+        candidatosMaisVotados = selectionSort(candidatosMaisVotados);
+
+        int size = candidatosMaisVotados.size() - 1;
+        for (int i = size; i >= 0; i--) {
+            Candidato c = candidatosMaisVotados.get(i);
+            System.out.print(c.id + " ");
+            System.out.printf("%.2f", c.pctDeVotosSegundoTurno * 100);
+            System.out.println();
+        }
     }
 
-    public void selectionSort() {
+    public List<Candidato> selectionSort(List<Candidato> candidatos) {
+        for (int i = 0; i < candidatos.size(); i++) {
+            int menor = i;
+            for (int j = i + 1; j < candidatos.size(); j++) {
+                Candidato candAtual = candidatos.get(j);
+                Candidato candmenor = candidatos.get(menor);
+                if (candAtual.votosRecebidosPrimeiroTurno < candmenor.votosRecebidosPrimeiroTurno) {
+                    menor = j;
+                }
+            }
 
+            Candidato aux = candidatos.get(menor);
+            candidatos.set(menor, candidatos.get(i));
+            candidatos.set(i, aux);
+        }
+        return candidatos;
+        // System.out.println(Candidatos);
     }
 
     public static void main(String[] args) {
