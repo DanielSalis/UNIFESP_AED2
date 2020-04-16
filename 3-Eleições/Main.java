377ed9c4029895571879965751815b11416fa980
Daniel Amorim Vilela de Salis <38293875+DanielSalis@users.noreply.github.com>
2020-04-15 21:46:44 -0300

    .

--- 3-Eleições/Main.java
+++ 3-Eleições/Main.java
@@ -90,13 +90,28 @@ public class Main {
         }
 
         List<Candidato> candOrdenados = selectionSort(Candidatos);
-        Candidato c = candOrdenados.get(candOrdenados.size()-1);
-        System.out.print(c.id + " ");
-        System.out.printf("%.2f", c.pctDeVotosPrimeiroTurno * 100);
-        System.out.println();
 
-        getResultadosSegundoTurno(candOrdenados.get(candOrdenados.size() - 1),
-        candOrdenados.get(candOrdenados.size() - 2));
+        Candidato cand1_ = candOrdenados.get(candOrdenados.size() - 1);
+        Candidato cand2_ = candOrdenados.get(candOrdenados.size() - 2);
+
+        if(cand1_.votosRecebidosPrimeiroTurno == cand2_.votosRecebidosPrimeiroTurno){
+            System.out.print(cand2_.id + " ");
+            System.out.printf("%.2f", cand2_.pctDeVotosPrimeiroTurno * 100);
+            System.out.println();
+    
+            getResultadosSegundoTurno(cand1_, cand2_);
+
+            return;
+        }
+        else{
+            Candidato c = candOrdenados.get(candOrdenados.size()-1);
+            System.out.print(c.id + " ");
+            System.out.printf("%.2f", c.pctDeVotosPrimeiroTurno * 100);
+            System.out.println();
+    
+            getResultadosSegundoTurno(candOrdenados.get(candOrdenados.size() - 1),
+            candOrdenados.get(candOrdenados.size() - 2));
+        }
 
     }
 
@@ -132,6 +147,20 @@ public class Main {
         }
 
         candidatosMaisVotados = selectionSortII(candidatosMaisVotados);
+
+        if(candidato_1.pctDeVotosSegundoTurno == candidato_2.pctDeVotosSegundoTurno){
+            Candidato cand = new Candidato();
+            if(candidato_1.id < candidato_2.id){
+                cand = candidato_1;
+            }else{
+                cand = candidato_2;
+            }
+            System.out.print(cand.id + " ");
+            System.out.printf("%.2f", cand.pctDeVotosSegundoTurno * 100);
+            System.out.println();
+
+            return;
+        }
 
         int size = candidatosMaisVotados.size() - 1;
 
