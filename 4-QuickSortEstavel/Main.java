d147c2bfa33bea3d0e54cd9a7cf5bb3fdd70778b
Daniel Salis <danielsalis98@gmail.com>
2020-04-20 22:43:39 -0300

    .

--- 4-QuickSortEstavel/Main.java
+++ 4-QuickSortEstavel/Main.java
@@ -14,6 +14,7 @@ public class Main {
     List<Pessoa> Pessoas = new ArrayList<Pessoa>();
     int posicaoNomeImpresso;
     int quantidadeNomesImpressos;
+    boolean caotic;
 
     public void setData(Scanner scanner) {
         int numeroPessoas = scanner.nextInt();
@@ -32,9 +33,7 @@ public class Main {
         quantidadeNomesImpressos = scanner.nextInt();
     }
 
-    public int encontrarPivo() {
-        int p = posicaoNomeImpresso - 1;
-        int r = Pessoas.size() - 1;
+    public int encontrarPivo(int p, int r) {
         int meio = (p + r) / 2;
 
         Pessoa a = Pessoas.get(p);
@@ -74,19 +73,20 @@ public class Main {
 
     public void quickSort(List<Pessoa> pessoas, int p, int r) {
         if (p < r) {
-            int j = separar(pessoas, p, r);
-            quickSort(pessoas, p, r - 1);
-            quickSort(pessoas, j + 1, r);
+            int q = separar(pessoas, p, r);
+            quickSort(pessoas, p, q - 1);
+            quickSort(pessoas, q + 1, r);
         }
     }
 
     public int separar(List<Pessoa> pessoas, int p, int r) {
+        int mediana = encontrarPivo(p, r);
         int i = p + 1;
         int j = r;
-        Pessoa pivo = Pessoas.get(p);
+        Pessoa pivo = pessoas.get(mediana);
         while (i <= j) {
-            Pessoa pi = Pessoas.get(i);
-            Pessoa pj = Pessoas.get(j);
+            Pessoa pi = pessoas.get(i);
+            Pessoa pj = pessoas.get(j);
             if (pi.idade <= pivo.idade)
                 i++;
             else if (pj.idade > pivo.idade)
@@ -103,17 +103,41 @@ public class Main {
     }
 
     public void trocar(List<Pessoa> pessoas, int i, int j) {
-        Pessoa pi = Pessoas.get(i);
-        Pessoa pj = Pessoas.get(j);
+        Pessoa pi = pessoas.get(i);
+        Pessoa pj = pessoas.get(j);
 
         Pessoa aux = pi;
         pessoas.set(i, pj);
         pessoas.set(j, aux);
     }
 
+    public void percorrerSemelhantes() {
+        int size = Pessoas.size() - 1;
+        int i;
+        for (i = 0; i < size; i++) {
+            Pessoa p1 = Pessoas.get(i);
+            Pessoa p2 = Pessoas.get(i + 1);
+            if (p1.idade == p2.idade) {
+                if (p2.posicaoInicial == i) {
+                    caotic = true;
+                    return;
+                }
+            }
+            caotic = false;
+        }
+    }
+
     public void imprimirPessoas() {
         for (Pessoa p : Pessoas) {
             System.out.println(p.nome + " " + p.idade);
+        }
+    }
+
+    public void imprimirSelecionados() {
+        int i;
+        for (i = posicaoNomeImpresso - 1; i < quantidadeNomesImpressos + posicaoNomeImpresso - 1; i++) {
+            Pessoa pi = Pessoas.get(i);
+            System.out.println(pi.nome + " " + pi.idade);
         }
     }
 
@@ -123,11 +147,17 @@ public class Main {
 
         main.setData(scanner);
 
-        int medianaIndice = main.encontrarPivo();
+        main.quickSort(main.Pessoas, 0, main.Pessoas.size() - 1);
 
-        main.quickSort(main.Pessoas, 3, main.Pessoas.size() - 1);
-
+        main.percorrerSemelhantes();
+        if (main.caotic) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+        // main.imprimirSelecionados();
         main.imprimirPessoas();
+        System.out.println();
 
         scanner.close();
     }
